4c8078751d9fe27daad0e6cac41b00ef89f55172
Mahesh Subramanian <mahesh.subramanian1@hmcts.net>
2025-09-18 10:57:38 +0100

    Addressing review comments

--- ai-document-answer-scoring-function/src/main/java/uk/gov/moj/cp/scoring/model/ChunkedEntry.java
+++ ai-document-answer-scoring-function/src/main/java/uk/gov/moj/cp/scoring/model/ChunkedEntry.java
@@ -2,49 +2,10 @@ package uk.gov.moj.cp.scoring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class ChunkedEntry {
-
-    @JsonProperty("documentFileName")
-    private String documentFileName;
-
-    @JsonProperty("chunk")
-    private String chunk;
-
-    @JsonProperty("pageNumber")
-    private Integer pageNumber;
-
-    @JsonProperty("score")
-    private double score;
-
-    public String getDocumentFileName() {
-        return documentFileName;
-    }
-
-    public void setDocumentFileName(String documentFileName) {
-        this.documentFileName = documentFileName;
-    }
-
-    public String getChunk() {
-        return chunk;
-    }
-
-    public void setChunk(String chunk) {
-        this.chunk = chunk;
-    }
-
-    public Integer getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(Integer pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
-    public Double getScore() {
-        return score;
-    }
-
-    public void setScore(final Double score) {
-        this.score = score;
-    }
+public record ChunkedEntry(
+        @JsonProperty("documentFileName") String documentFileName,
+        @JsonProperty("chunk") String chunk,
+        @JsonProperty("pageNumber") Integer pageNumber,
+        @JsonProperty("score") double score
+) {
 }
--- ai-document-answer-scoring-function/src/main/java/uk/gov/moj/cp/scoring/model/QueryResponse.java
+++ ai-document-answer-scoring-function/src/main/java/uk/gov/moj/cp/scoring/model/QueryResponse.java
@@ -4,51 +4,10 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class QueryResponse {
-
-    @JsonProperty("userQuery")
-    private String userQuery;
-
-    @JsonProperty("llmResponse")
-    private String llmResponse;
-
-    @JsonProperty("queryPrompt")
-    private String queryPrompt;
-
-    @JsonProperty("chunkedEntries")
-    private List<ChunkedEntry> chunkedEntries;
-
-    public String getUserQuery() {
-        return userQuery;
-    }
-
-    public void setUserQuery(final String userQuery) {
-        this.userQuery = userQuery;
-    }
-
-    public String getLlmResponse() {
-        return llmResponse;
-    }
-
-    public void setLlmResponse(final String llmResponse) {
-        this.llmResponse = llmResponse;
-    }
-
-    public String getQueryPrompt() {
-        return queryPrompt;
-    }
-
-    public void setQueryPrompt(final String queryPrompt) {
-        this.queryPrompt = queryPrompt;
-    }
-
-    public List<ChunkedEntry> getChunkedEntries() {
-        return chunkedEntries;
-    }
-
-    public void setChunkedEntries(final List<ChunkedEntry> chunkedEntries) {
-        this.chunkedEntries = chunkedEntries;
-    }
-
-
+public record QueryResponse(
+        @JsonProperty("userQuery") String userQuery,
+        @JsonProperty("llmResponse") String llmResponse,
+        @JsonProperty("queryPrompt") String queryPrompt,
+        @JsonProperty("chunkedEntries") List<ChunkedEntry> chunkedEntries
+) {
 }
