084cdedc9a3240b4ceb3af088f075c717a506019
Rohit Prashar <rohit.prashar1@hmcts.net>
2025-10-07 17:16:13 +0100

    DD-40122 Implement function to ingest message queue from queue storage

--- ai-document-ingestion-function/src/main/java/uk/gov/moj/cp/ingestion/exception/QueueMessageParsingException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.moj.cp.ingestion.exception;
-
-public class QueueMessageParsingException extends Exception {
-
-    public QueueMessageParsingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
