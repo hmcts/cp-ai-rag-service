c50b726a6e09091dbbc467d1a6ca190d2af4979e
Natraj Molala <natraj.molala3@hmcts.net>
2026-02-19 20:19:46 +0000

    initial draft document upload endpoint

--- ai-document-metadata-check-function/src/main/java/uk/gov/moj/cp/metadata/check/service/IngestionOrchestratorService.java
+++ ai-document-metadata-check-function/src/main/java/uk/gov/moj/cp/metadata/check/service/IngestionOrchestratorService.java
@@ -85,7 +85,7 @@ public class IngestionOrchestratorService {
     }
 
 
-    public boolean isDocumentAlreadyProcessed(final String documentName) {
+    private boolean isDocumentAlreadyProcessed(final String documentName) {
         try {
             final DocumentIngestionOutcome firstDocumentMatching = documentIngestionOutcomeTableService.getFirstDocumentMatching(documentName);
             if (null != firstDocumentMatching) {
@@ -122,16 +122,5 @@ public class IngestionOrchestratorService {
         documentIngestionOutcomeTableService.insertIntoTable(documentName, effectiveDocumentId, status, reason);
 
         LOGGER.info("Status for document '{}' with ID '{}' updated to '{}'", documentName, effectiveDocumentId, status);
-    }
-
-    /**
-     * Records a document ingestion outcome (success or failure) in Table Storage.
-     */
-    public void recordUploadInitiated(final String documentName, final String documentId) throws DuplicateRecordException {
-        final String effectiveDocumentId = isNullOrEmpty(documentId) ? UNKNOWN_DOCUMENT : documentId.trim();
-
-        documentIngestionOutcomeTableService.insertUploadInitiated(documentName, effectiveDocumentId, METADATA_VALIDATED.name(), METADATA_VALIDATED.getReason());
-
-        LOGGER.info("Status for document '{}' with ID '{}' updated to '{}'", documentName, effectiveDocumentId, METADATA_VALIDATED.name());
     }
 }
--- ai-document-shared-artefacts/src/main/java/uk/gov/moj/cp/ai/model/BlobMetadataAttributes.java
+++ ai-document-shared-artefacts/src/main/java/uk/gov/moj/cp/ai/model/BlobMetadataAttributes.java
@@ -3,5 +3,5 @@ package uk.gov.moj.cp.ai.model;
 public class BlobMetadataAttributes {
 
     public static final String DOCUMENT_ID = "document_id";
-    public static final String DOCUMENT_UPLOAD_DATE = "document_upload_date";
+    public static final String BLOB_CORE_URI_STR = "https://%s.blob.core.windows.net";
 }
